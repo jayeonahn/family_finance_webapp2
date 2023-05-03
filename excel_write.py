54e30103cf432b2256a6df538ab95ad788a8af42
jayeo <jayeon.ahn@gmail.com>
2023-05-03 15:13:20 -0400

    -Cleaned up all, used pandas, need to write code for writing to excel

--- excel_write.py
+++ excel_write.py
@@ -1,22 +1,25 @@
 #from csv import DictWriter
+from openpyxl import Workbook, load_workbook
+from pandas import read_excel
 import os
 
+FILEPATH = "family_finance_master.xlsx"
+
+#ref_sheet = work_book['Ref_Categories']
+
+#work_book = load_workbook(FILEPATH)
+#main_sheet = work_book['ITEMS']
+ref_cat = read_excel(FILEPATH, index_col=0, sheet_name="Ref_Categories")
+ref_pay = read_excel(FILEPATH, header=0, sheet_name="Payment Methods")
+
+
+def add_item_to_excel(dict_list):
+    pass
 """
-item_dict = {'date': '4/3/2023', 'category': 'eating out', 'description': 'dinner at Uno'}
-"""
-
-FILEPATH = "family_expenses.xlsx"
-
-
-
-HEADERS = ["date", "category", "description", "amount", "payment_method", "online", "main_category"]
-print("cvs.headers", HEADERS)
-    #["date", "category", "main_category", "description", "description", "amount", "payment_method", "online", "notes"]
-
 if not os.path.exists(FILEPATH):
     with open(FILEPATH,"w", newline="") as file:
-        file = DictWriter(file, fieldnames = HEADERS)
-        file.writeheader()
+        file = DictWriter(file, fieldnames = HEADERS)  #create a new file
+        file.writeheader()          #write headers on that file
 
 def add_item_row(item_dict):
     with open(FILEPATH, "a", newline="") as file:
@@ -24,3 +27,20 @@ def add_item_row(item_dict):
         print("dict_file: ")
         print(dict_file)
         dict_file.writerow(item_dict)
+"""
+
+""" To retrieve data from the excel file (using pandas)"""
+def get_sub_categories(category):
+    return ref_cat[ref_cat["Category"] == category].index.tolist()
+
+def get_main_categories():
+    return ref_cat['Category'].drop_duplicates().values.tolist()
+
+def get_payment_methods():
+    return ref_pay['payment_methods'].tolist()
+
+
+
+
+
+
--- web.py
+++ web.py
@@ -2,27 +2,31 @@ import streamlit as st
 #import category_lookup as cl
 import datetime
 import pandas
-#import csv_write as cw
+import excel_write as ew
 #import category_lookup
 
 st.title("Family Expenses")
 st.write("Keeping a record of expenses is <b>good</b> practice.",
          unsafe_allow_html=True)  #HTML enabled only for write function
 
+
 def add_item():
-    dict_row = {}
-    dict_row["description"] = st.session_state["description"]
-    dict_row["date"] = st.session_state["date"]
-    dict_row["amount"] = st.session_state["amount"]
-    dict_row["payment_method"] = st.session_state["payment_method"]
-    dict_row["online"] = st.session_state["online"]
-    dict_row["main_category"] = st.session_state["main_category"]
-    dict_row["category"] = st.session_state["category"]
+    item_dict = {}
+    dt = datetime.datetime.now()
+    time_stamp = dt.strftime("%m-%d-%Y %I:%M%p")
+    item_dict["Timestamp"] = time_stamp
+    item_dict["description"] = st.session_state["description"]
+    item_dict["date"] = st.session_state["date"]
+    item_dict["amount"] = st.session_state["amount"]
+    item_dict["payment_method"] = st.session_state["payment_method"]
+    item_dict["online"] = st.session_state["online"]
+    item_dict["main_category"] = st.session_state["main_category"]
+    item_dict["category"] = st.session_state["sub_category"]
 
-    print(dict_row)
-    #cw.add_item_row(dict_row)
-
+    # cw.add_item_row(dict_row)
+    ew.add_item_to_excel(item_dict)
     st.write("**:tada: :blue[You have successfully submitted an item. Type in a new item]**")
+    st.write(item_dict)
     #st.session_state["form_date"] = st.session_state["form_date"].today()
     #st.session_state["amount"] = ""
     #del st.session_state["amount"]
@@ -33,34 +37,28 @@ item_date = st.date_input(label="**:calendar: :blue[Enter date: ]**",
 st.session_state["date"] = item_date.strftime('%m/%d/%Y')
 
 #categories_list = cl.get_main_categories()
-main_categories = ["thing one", "thing two", "thing three"]
-category_selected = st.selectbox(label="**:white_check_mark: Select a category:**",
+
+main_categories = ew.get_main_categories()
+Category_selected = "Select a category"
+main_categories.insert(0, "Select a category")
+category_selected = st.selectbox(label="**:lower_left_ballpoint_pen: Select a category:**",
                                  options=main_categories, key="main_category")
 #options (Sequence, numpy.ndarray, pandas.Series, pandas.DataFrame, or pandas.Index)
 
-#subcategories = cl.look_up_subcategory(category_selected)
-if category_selected == "thing one":
-    subcategories = ["thing a", "thing b", "thing c"]
-elif category_selected == "thing two":
-    subcategories = ["two a", "two b", "two c"]
-else: subcategories = ["three a", "three b", "three c"]
+if (category_selected != "Select a category"):
+    sub_categories = ew.get_sub_categories(category_selected)
+    sub_categories.insert(0, "Select a subcategory")
+    sub_category_selected = st.selectbox(label="**:lower_left_ballpoint_pen: Select a sub-category:**",
+                                         options=sub_categories, key="sub_category")
 
-st.selectbox(label="**:calendar: Select a sub-category:**",
-             options=subcategories, key="category")
-
-#categories_list = cl.get_subcategories()
-#category_selected = st.selectbox(label=":white_check_mark: Select a category:",
-#                           options=categories_list, key="category")
-
-st.text_input(label="**:page_facing_up: Enter Description:**",
+st.text_input(label="**:page_facing_up: Description:**",
               placeholder="what, where, why...",
               key="description")
 
-st.number_input(label="**:heavy_dollar_sign: Enter Amount:**",
+amount = st.number_input(label="**:heavy_dollar_sign: Enter Amount:**",
                 value = 0, key="amount")
 
-#payment_methods = cl.get_payment_methods()
-payment_methods = ["AmEx", "CapitalOne", "Cash"]
+payment_methods = ew.get_payment_methods()
 st.radio(label="**:credit_card: Select payment method:**",
          index=0, options = payment_methods, key="payment_method")
 
@@ -70,9 +68,14 @@ st.checkbox(label="yes", value=False, key="online")
 submit_button = st.button(label="-SUBMIT-", type="primary", key="submit")  # --need this? on_click=add_item
 
 if submit_button:
-    add_item()
-    st.experimental_rerun()
+    if category_selected == "Select a category":
+        st.write(":broken_heart: :red[You must enter a category.]")
+    elif sub_category_selected == "Select a subcategory":
+        st.write(":broken_heart: :red[You must enter a subcategory.]")
+    elif st.session_state["amount"] == 0:
+        st.write(":broken_heart: :red[You must enter an amount.]")
+    else: add_item()
+    #st.experimental_rerun()
 
-st.write(submit_button)
 
 
